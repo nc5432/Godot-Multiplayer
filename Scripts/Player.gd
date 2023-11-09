ada7ca75083d0a53444576490f5977537364f201
Nolan A Casey <ncasey113@gmail.com>
2023-11-08 22:00:52 -0500

    Finished following tutorial

---
+++ Scripts/Player.gd
@@ -0,0 +1,85 @@
+extends CharacterBody3D
+
+signal healthChanged(change)
+
+const SPEED: float = 10.0
+const JUMP_VELOCITY: float = 10
+
+@onready var camera: Camera3D = $Camera3D
+@onready var animations: AnimationPlayer = $AnimationPlayer
+@onready var flash = $Camera3D/gun/MuzzleFlash
+@onready var raycast = $Camera3D/RayCast3D
+
+@export var lookSensitivity: float = 0.005
+@export var health: float = 3
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = 20
+
+func _ready():
+	if not is_multiplayer_authority(): return
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
+
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * lookSensitivity)
+		camera.rotate_x(-event.relative.y * lookSensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		
+	if Input.is_action_just_pressed("shoot") and animations.current_animation != "shoot":
+		shoot.rpc()
+		if raycast.is_colliding():
+			var hit_player = raycast.get_collider()
+			hit_player.receiveDamage.rpc_id(hit_player.get_multiplayer_authority())
+			
+
+@rpc("call_local")
+func shoot():
+	animations.stop()
+	animations.play("shoot")
+	flash.restart()
+	flash.emitting = true
+
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if animations.current_animation != "shoot":
+		if input_dir != Vector2.ZERO and is_on_floor():
+			animations.play("move")
+		else:
+			animations.play("idle")
+
+	move_and_slide()
+
+@rpc("any_peer")
+func receiveDamage():
+	health -= 1
+	if health <= 0:
+		position = Vector3.ZERO
+		health = 3
+	healthChanged.emit(health)
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "shoot":
+		animations.play("idle")
---
+++ Scripts/world.gd
@@ -0,0 +1,71 @@
+extends Node
+
+@onready var mainMenu = $CanvasLayer/MainMenu
+@onready var addressEntry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+@onready var hud = $CanvasLayer/HUD
+@onready var healthbar = $CanvasLayer/HUD/HealthBar
+
+const Player = preload("res://Prefabs/player.tscn")
+const PORT = 2571
+var enet_peer = ENetMultiplayerPeer.new()
+
+@export var spawnpoint: Vector3 = Vector3(0, 6.376, 0)
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func _on_host_button_pressed():
+	mainMenu.hide()
+	hud.show()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(addPlayer)
+	multiplayer.peer_disconnected.connect(removePlayer)
+	addPlayer(multiplayer.get_unique_id())
+	upnpSetup()
+
+func _on_join_button_pressed():
+	mainMenu.hide()
+	hud.show()
+	
+	enet_peer.create_client(addressEntry.text, PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func addPlayer(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	player.position = spawnpoint
+	add_child(player)
+	if player.is_multiplayer_authority():
+		player.healthChanged.connect(updateHealthBar)
+
+func removePlayer(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
+func updateHealthBar(health):
+	healthbar.value = health
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.healthChanged.connect(updateHealthBar)
+
+func upnpSetup():
+	var upnp = UPNP.new()
+	var discoverResult = upnp.discover()
+	assert(discoverResult == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover Failed! Error %s" % discoverResult)
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway!")
+	var mapResult = upnp.add_port_mapping(PORT)
+	assert(mapResult == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping Failed! Error %s" % mapResult)
+	print("Success! Join Address: %s" % upnp.query_external_address())
+
+
+
+
+
+
+
+
